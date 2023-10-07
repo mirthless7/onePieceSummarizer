59ada765eea4797cfdc60ad20efdb51ff3296eee
Bryan <bryanleyva2005@gmail.com>
2023-10-07 16:20:52 -0400

    main changes to list updated

---
+++ bryanhehe.py
@@ -0,0 +1,63 @@
+def arc_name(ep_number):
+    if(ep_number>0 and ep_number<4):
+        return "Romance Dawn"
+    elif (ep_number>3 and ep_number<9):
+        return "Orange Town"
+    elif (ep_number>8 and ep_number<19):
+        return "Syrup Village"
+    elif (ep_number>3 and ep_number<9):
+        return "Baratie"
+    elif (ep_number>8 and ep_number<19):
+        return "Arlong Park"
+    elif (ep_number>3 and ep_number<9):
+        return "Loguetown"
+    elif (ep_number>3 and ep_number<9):
+        return "Reverse Mountain"
+    elif (ep_number>8 and ep_number<19):
+        return "Whisky Peak"
+    elif (ep_number>8 and ep_number<19):
+        return "Little Garden"
+    elif (ep_number>3 and ep_number<9):
+        return "Drum Island"
+    elif (ep_number>8 and ep_number<19):
+        return "Alabasta"
+    elif (ep_number>3 and ep_number<9):
+        return "Jaya"
+    elif (ep_number>8 and ep_number<19):
+        return "Skypiea"
+    elif (ep_number>3 and ep_number<9):
+        return "Long Ring Long Land"
+    elif (ep_number>8 and ep_number<19):
+        return "Water 7"
+    elif (ep_number>3 and ep_number<9):
+        return "Enies Lobby"
+    elif (ep_number>8 and ep_number<19):
+        return "Post-Enies Lobby"
+    elif (ep_number>8 and ep_number<19):
+        return "Thriller Bark"
+    elif (ep_number>8 and ep_number<19):
+        return "Sabaody Archipelago"
+    elif (ep_number>8 and ep_number<19):
+        return "Amazon Lily"
+    elif (ep_number>8 and ep_number<19):
+        return "Impel Down"
+    elif (ep_number>8 and ep_number<19):
+        return "Marineford"
+    elif (ep_number>8 and ep_number<19):
+        return "Post-War"
+    elif (ep_number>8 and ep_number<19):
+        return "Return to Sabaody"
+    elif (ep_number>8 and ep_number<19):
+        return "Fishman Island"
+    elif (ep_number>8 and ep_number<19):
+        return "Punk Hazard"
+    elif (ep_number>8 and ep_number<19):
+        return "Dressrosa"
+    elif (ep_number>8 and ep_number<19):
+        return "Zou"
+    elif (ep_number>8 and ep_number<19):
+        return "Whole Cake Island"
+    elif (ep_number>8 and ep_number<19):
+        return "Levely"
+    elif (ep_number>8 and ep_number<19):
+        return "Wano Country"
--- main.py
+++ main.py
@@ -6,12 +6,9 @@ import requests
 import os
 
 CATALOGUE = ["Romance Dawn", "Orange Town", "Syrup Village", "Baratie", "Arlong Park", "Logue Town", "Buggy's Crew Adventure Chronicles", "Warship Island", 
-            "Reverse Mountain", "Whisky Peak", "Diary of Koby-Meppo", "Little Garden", "Drum Island", "Alabasta", "Post-Arabasta", 
-            "Goat Island", "Ruluka Island", "Jaya", "Skypiea", "G-8","Long Ring Long Land", "Ocean's Dream", "Foxy's Return", "Water 7", "Enies Lobby",
-            "Boss Luffy Historical", "Post-Enies Lobby", "Ice Hunter", "Chopper Man Special", "Thriller Bark", "Spa Island", "Sabaody Archipelago",
-            "Amazon Lily", "Straw Hat's Separation Serial", "Impel Down", "Little East Blue", "Marineford", "Post-War", "Toriko Crossover", "Return to Sabaody",
-            "Fish-Man Island", "Toriko Crossover", "Z's Ambition", "Punk Hazard", "Toriko & Dragon Ball Crossover", "Caesar Retrieval", "Dressrosa", "Silver Mine", 
-            "Zou", "Marine Rookie", "Whole Cake Island", "Levely","Wano Country", "Cidre Guild", "Anime 20th Anniversary special", "Uta's Past", "Egghead"]
+            "Reverse Mountain", "Whisky Peak", "Little Garden", "Drum Island", "Alabasta","Jaya", "Skypiea", "Long Ring Long Land", "Water 7", "Enies Lobby",
+            "Post-Enies Lobby", "Thriller Bark","Sabaody Archipelago","Amazon Lily", "Impel Down","Marineford", "Post-War","Return to Sabaody",
+            "Fish-Man Island","Punk Hazard","Dressrosa","Zou","Whole Cake Island", "Levely","Wano Country"]
 
 
 #https://dzone.com/articles/how-to-paraphrase-text-in-python-using-nlp-librari#:~:text=In%20Python%2C%20there%20are%20machine,are%20%E2%80%9CDeep%20learning%E2%80%9D%20models.
