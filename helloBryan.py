24957dd303575f51ccb3b3f34fa01f935ebc0200
wettestsock <evilsharkeater@gmail.com>
2023-10-07 02:02:37 -0400

    grfdg

--- helloBryan.py
+++ helloBryan.py
@@ -1,1 +1,8 @@
-print("hello")
+import pandas as pd
+import numpy as npy
+import scipy as spy
+
+#https://dzone.com/articles/how-to-paraphrase-text-in-python-using-nlp-librari#:~:text=In%20Python%2C%20there%20are%20machine,are%20%E2%80%9CDeep%20learning%E2%80%9D%20models.
+
+titles = pd.read_csv('onePiece_title.csv')
+print(titles)
