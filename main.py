c121f16c1cb962738543500e68f7e293f5aa66f2
wettestsock <evilsharkeater@gmail.com>
2023-10-07 14:51:53 -0400

    Merge branch

--- main.py
+++ main.py
@@ -12,29 +12,42 @@ url='https://raw.githubusercontent.com/mirthless7/onePieceSummarizer/main/idkman
 #page = requests.get(url)
 #print(page.text)
 
-lowB=0;
-upB=1;
+import pandas as pd
+import numpy as npy
+import scipy as spy
+import pysrt
+import requests
+import os
 
-print("ONE PIECE SUMMARIZER\n by Ronan Buck, Bryan Leyva, and Dmytro Moshkovskyi\n")
+#https://dzone.com/articles/how-to-paraphrase-text-in-python-using-nlp-librari#:~:text=In%20Python%2C%20there%20are%20machine,are%20%E2%80%9CDeep%20learning%E2%80%9D%20models.
+
+url='https://raw.githubusercontent.com/mirthless7/onePieceSummarizer/main/idkman.txt'
+
+#page = requests.get(url)
+#print(page.text)
+
+
+vs={'lowBound_page': 0, 'upBound_page': 1,
+    'lowBound_saga': 0, 'lowBound_arc': 0,
+    'upBound_saga': 0, 'upBound_arc':0}
+
+
+print("ONE PIECE SUMMARIZER\n by Ronan Buck, Brian Leyva, and Dmytro Moshkovskyi\n")
 
 
 while True:
     try:
-        lowB=int(input("Which episode number to start from: "))
+        lowBound_page=int(input("Which episode number to start from: "))
         break
     except:
         print("That's not a valid episode number")
     
 while True:
     try:
-        upB=int(input("Which episode number to end with: "))
+        upBound_page=int(input("Which episode number to end with: "))
         break
     except:
         print("That's not a valid episode number")
-        
-
-
-
 
 
 
