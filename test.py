b33bed2c92b42fcb2bfcc7052665c859da94c95a
Bryan <bryanleyva2005@gmail.com>
2023-10-07 14:21:06 -0400

    hgfhgiefwhegwd

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import csv
-
-
-page = pd.read_csv('https://raw.githubusercontent.com/mirthless7/onePieceSummarizer/main/bible.csv', delimiter='\t')
-
-print(page.iloc[3:5+1, 0].values.tolist())
-
-print(page)
