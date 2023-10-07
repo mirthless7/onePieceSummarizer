de9c4b764d52c94d9b8ad078375a1eb77a73f9eb
wettestsock <evilsharkeater@gmail.com>
2023-10-07 17:23:46 -0400

    jdgfd

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
 from heapq import nlargest
 
+nlp = spacy.load("en_core_web_sm")
+nlp.Defaults.stop_words.add('\n')
+
 CATALOGUE = ["Romance Dawn", "Orange Town", "Syrup Village", "Baratie", "Arlong Park", "Logue Town","Reverse Mountain", "Whisky Peak", "Little Garden", "Drum Island", "Alabasta","Jaya", "Skypiea", "Long Ring Long Land", "Water 7", "Enies Lobby",
             "Post-Enies Lobby", "Thriller Bark","Sabaody Archipelago","Amazon Lily", "Impel Down","Marineford", "Post-War","Return to Sabaody",
             "Fish-Man Island","Punk Hazard","Dressrosa","Zou","Whole Cake Island", "Levely","Wano Country", "746"]
@@ -17,8 +20,8 @@ url='https://raw.githubusercontent.com/mirthless7/onePieceSummarizer/main/idkman
 
 
 
-lowB=0;
-upB=1;
+lowB=0
+upB=1
 
 print("ONE PIECE SUMMARIZER\n by Ronan Buck, Bryan Leyva, and Dmytro Moshkovskyi\n")
 
@@ -45,9 +48,9 @@ while True:
 lowArc=arc_name(lowB) #PLACEHOLDER
 upArc=arc_name(upB)
 
-for nextArc in CATALOGUE:
-    if (nextArc==upArc):
-        upArc=nextArc
+for i in range(0,32):
+    if (upArc==CATALOGUE[i]):
+        upArc=CATALOGUE[i+1]
         break
 
 if (lowArc=='Not Found' or upArc=='Not Found'):
@@ -64,10 +67,13 @@ with open('one-piece-the-book.txt', 'r', encoding="utf-8") as book:
             output = output + line 
 
         if lowArc in line[0:20]:
+            if (add==True):
+                continue
             add = True 
 
         elif upArc in line[0:20]:
             break
 
-print(summarize(output,0.01))
+
+print(f'----------\nONE PIECE SUMMARY:\n\n-- ARCS: {lowArc} to {arc_name(upB)}\n-----------\n{summarize(output,0.01)}\n\n')
             
