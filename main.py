c02b46c6d9be5623e8ab25d32d168f9989dfd8cb
wettestsock <evilsharkeater@gmail.com>
2023-10-07 16:05:44 -0400

    dfjkgfs

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as npy
 import scipy as spy
-import pysrt
-import requests
 import os
 
 CATALOGUE = {
@@ -26,12 +24,6 @@ url='https://raw.githubusercontent.com/mirthless7/onePieceSummarizer/main/idkman
 #page = requests.get(url)
 #print(page.text)
 
-import pandas as pd
-import numpy as npy
-import scipy as spy
-import pysrt
-import requests
-import os
 
 #https://dzone.com/articles/how-to-paraphrase-text-in-python-using-nlp-librari#:~:text=In%20Python%2C%20there%20are%20machine,are%20%E2%80%9CDeep%20learning%E2%80%9D%20models.
 
@@ -64,15 +56,30 @@ while True:
         print("That's not a valid episode number")
         
 tempLowArc='Orange Town'
-tempUpArc= 'Goat Island'
+tempUpArc= 'Arlong Park'
 
 output=' '
 with open('one-piece-the-book.txt', 'r', encoding="utf-8") as book:
+    add = False
     for line in book:
-        if 'the' in line[0:10]:
-            output = output+line
+        if add == True: 
+            if tempLowArc in line[0:20]:
+                continue
+            output = output + line + '\n'
+
+        if tempLowArc in line[0:20]:
+            add = True 
+        elif tempUpArc in line[0:20]:
+            break
+
+print(output)
+            
+        
+
+            
 
 #print(output)
+
 def arc_name(ep_number):
     if(ep_number>0 and ep_number<4):
         return "Romance Dawn"
